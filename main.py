666cf6775ddb1d203b264acd2974bf5286cf6961
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2023-09-27 14:52:54 -0400

    Stevan est commit

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 #cree par Tristan Matteau en 2023
-#tp 2
+#tp 2.0
 
 import random
 
 nbr_random = random.randint(0, 1000)
 print(nbr_random)
 
-print("J’ai choisi un nombre au hasard entre borne_minimal et borne_maximal.")
+print("J’ai choisi un nombre au hasard entre 0 et 1000.")
 print("À vous de le deviner...")
 
-
 question = int(input("Entrez votre essai: "))
-print(question)
 
-if question > nbr_random:
-    print("esseye plus petit")
-    question = input(str("Entrez votre essai: "))
-    print(question)
+essaie = 0
 
-elif question == nbr_random:
-    print("oui")
-    exit()
+
+
+while question != nbr_random:
+    essaie +=1
+    if question > nbr_random:
+        print("essaie plus petit")
+        print("nombre d'essaie: ", essaie)
+        question = int(input("Réessaye: "))
+
+    elif question < nbr_random:
+        print("essaie plus grand")
+        print("nombre d'essaie: ", essaie)
+        question = int(input("Réessaye: "))
+
+    elif question == nbr_random:
+        print("oui")
+        question = int(input("Réessaye:"))
 
 else:
-    print("esseye plus grand")
-    question = input(str("Entrez votre essai: "))
-    print(question)
+    exit()
+
+
+
