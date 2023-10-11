7489bada0f4a711d00a3527b295327c137858f71
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2023-10-11 10:46:00 -0400

    fhtsjj

--- main.py
+++ main.py
@@ -4,35 +4,42 @@
 import random
 
 nbr_random = random.randint(0, 1000)
-print(nbr_random)
+
+rejoue =
 
 print("J’ai choisi un nombre au hasard entre 0 et 1000.")
 print("À vous de le deviner...")
 
 question = int(input("Entrez votre essai: "))
-
-essaie = 0
+essai = 0
 
 
 
 while question != nbr_random:
-    essaie +=1
+    essai = essai+1
     if question > nbr_random:
         print("essaie plus petit")
-        print("nombre d'essaie: ", essaie)
+        print("nombre d'essaie: ", essai)
         question = int(input("Réessaye: "))
 
     elif question < nbr_random:
         print("essaie plus grand")
-        print("nombre d'essaie: ", essaie)
+        print("nombre d'essaie: ", essai)
         question = int(input("Réessaye: "))
 
-    elif question == nbr_random:
-        print("oui")
-        question = int(input("Réessaye:"))
-
 else:
-    exit()
+    print(nbr_random, "Bonne Reponse")
+    print("nombre d'essaie: ", essai +1)
+    print("voule-vous", quitter, "ou", rejouer,)
+    question2 = int(input("entrer votre choix: "))
 
+    if question2:
+        print("quitter")
+        exit()
 
+    elif print("rejouer"):
+        rejoue
 
+    else:
+        print("error!")
+        exit()
