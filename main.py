2e06e40ef8cb2f616d853bc169d9628c53c4a4d6
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2023-10-12 16:12:31 -0400

    g

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 nbr_random = random.randint(0, 1000)
 
-rejoue =
+rejouer = True
 
 print("J’ai choisi un nombre au hasard entre 0 et 1000.")
 print("À vous de le deviner...")
@@ -13,33 +13,45 @@ print("À vous de le deviner...")
 question = int(input("Entrez votre essai: "))
 essai = 0
 
+quitter = exit()
+
+while rejouer == True:
+        while question != nbr_random:
+            essai = essai+1
+            if question > nbr_random:
+                print("essaie plus petit")
+                print("nombre d'essaie: ", essai)
+                question = int(input("Réessaye: "))
+
+            elif question < nbr_random:
+                print("essaie plus grand")
+                print("nombre d'essaie: ", essai)
+                question = int(input("Réessaye: "))
+
+        else:
+            print(nbr_random, "Bonne Reponse")
+            print("nombre d'essaie: ", essai +1)
+            print("voule-vous quitter ou rejouer",)
 
 
-while question != nbr_random:
-    essai = essai+1
-    if question > nbr_random:
-        print("essaie plus petit")
-        print("nombre d'essaie: ", essai)
-        question = int(input("Réessaye: "))
+            question2 = str(input("entrer votre choix: "))
 
-    elif question < nbr_random:
-        print("essaie plus grand")
-        print("nombre d'essaie: ", essai)
-        question = int(input("Réessaye: "))
+            if question2 == quitter:
+                print("quitter")
+                exit()
 
-else:
-    print(nbr_random, "Bonne Reponse")
-    print("nombre d'essaie: ", essai +1)
-    print("voule-vous", quitter, "ou", rejouer,)
-    question2 = int(input("entrer votre choix: "))
+            elif question2 == rejouer:
+                print("rejouer")
 
-    if question2:
-        print("quitter")
-        exit()
 
-    elif print("rejouer"):
-        rejoue
+            else:
+                print("error!")
+                exit()
 
-    else:
-        print("error!")
-        exit()
+            if question2 == quitter:
+                exit()
+
+            else:
+                print("error!")
+                exit()
+
