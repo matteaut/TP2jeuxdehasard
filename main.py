334fbd50ecdfe91f41d52d568fa990d1d890fc7c
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2023-10-20 10:17:19 -0400

    ggreswtrty

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
 #cree par Tristan Matteau en 2023
 #tp 2.0
 
-
 import random
-
-nbr_random = random.randint(0, 1000)
 
 rejouer = True
 
-print("J’ai choisi un nombre au hasard entre 0 et 1000.")
-print("À vous de le deviner...")
+while rejouer:
+    nbr_random = random.randint(0, 1000)
 
-question = int(input("Entrez votre essai: "))
-essai = 0
-quitter = exit()
 
-while rejouer == True:
-        while question != nbr_random:
-            essai = essai+1
-            if question > nbr_random:
-                print("essaie plus petit")
-                print("nombre d'essaie: ", essai)
-                question = int(input("Réessaye: "))
 
-            elif question < nbr_random:
-                print("essaie plus grand")
-                print("nombre d'essaie: ", essai)
-                question = int(input("Réessaye: "))
+    print("J’ai choisi un nombre au hasard entre 0 et 1000.")
+    print("À vous de le deviner...")
+
+    question = int(input("Entrez votre essai: "))
+    essai = 0
+    reponse = True
+
+    while reponse:
+        essai = essai+1
+        if question > nbr_random:
+            print("essaie plus petit")
+            print("nombre d'essaie: ", essai)
+            question = int(input("Réessaye: "))
+
+        elif question < nbr_random:
+            print("essaie plus grand")
+            print("nombre d'essaie: ", essai)
+            question = int(input("Réessaye: "))
 
         else:
             print(nbr_random, "Bonne Reponse")
-            print("nombre d'essaie: ", essai +1)
-            print("voule-vous quitter ou rejouer",)
+            print("nombre d'essais: ", essai +1)
+            print("voule-vous quitter ou rejouer")
+            question2 = str(input("entrer votre choix 'Y' ou 'N': "))
+            reponse = False
 
-
-            question2 = str(input("entrer votre choix: "))
-
-            if question2 == quitter:
-                print("quitter")
+            if question2 == "N":
+                print("Bye Bye")
                 exit()
 
-            elif question2 == rejouer:
-                print("rejouer")
+            elif question2 == 'Y':
+                print("OK")
+                rejouer = True
 
 
-            else:
-                print("error!")
-                exit()
 
-            if question2 == quitter:
-                exit()
+
 
             else:
                 print("error!")
